35acf1f29fafa434b39de68968bbaddaf9f123f6
jeanyjsu <77689370+jeanyjsu@users.noreply.github.com>
2021-03-17 15:00:52 -0700

    the second question

---
+++ src/TheCircleCenter.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class TheCircleCenter {
+     public static void main(String[] args){
+          int width, height, blockWidth, blockHeight;
+          System.out.println("Please enter a width for the block: ");
+          Scanner scan = new Scanner(System.in);
+          width = scan.nextInt();
+          System.out.println("Please enter a height fot the block: ");
+          height = scan.nextInt();
+          height = scan.nextInt();
+     }
+}
---
+++ src/ThePlusBlock.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ThePlusBlock {
+     public static void main(String[] args){
+          int width, height, blockWidth, blockHeight;
+          System.out.println("Please enter a width for the block: ");
+          Scanner scan = new Scanner(System.in);
+          width = scan.nextInt();
+          System.out.println("Please enter a height fot the block: ");
+          height = scan.nextInt();
+
+          for(blockWidth= 1; blockWidth <= width; blockWidth++){
+               for(blockHeight = 1; blockHeight <= height; blockHeight++){
+                    if((blockWidth == width / 2) || (blockHeight == height/2)){
+                    System.out.print(" ");
+                    }else{
+                         System.out.print("#");
+                         System.out.print("");
+                    }
+               }
+               System.out.println("");
+          }
+          scan.close();
+     }
+}
