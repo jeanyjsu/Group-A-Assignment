10c642ec35290546ec5a48b8da98e22a142e2483
Bony3000 <donugha@gmail.com>
2021-03-18 13:49:17 -0700

    Update ThePlusBlock.java

--- src/ThePlusBlock.java
+++ src/ThePlusBlock.java
@@ -5,17 +5,19 @@ public class ThePlusBlock {
           System.out.println("Please enter a width for the block: ");
           Scanner scan = new Scanner(System.in);
           width = scan.nextInt();
-          System.out.println("Please enter a height fot the block: ");
+          System.out.println("Please enter a height for the block: ");
           height = scan.nextInt();
 
           for(blockWidth= 1; blockWidth <= width; blockWidth++){
                for(blockHeight = 1; blockHeight <= height; blockHeight++){
                     if((blockWidth == width / 2) || (blockHeight == height/2)){
                     System.out.print(" ");
-                    }else{
-                         System.out.print("#");
-                         System.out.print("");
                     }
+                    else
+                         {
+                              System.out.print("#");
+                              System.out.print("#");
+                         }
                }
                System.out.println("");
           }
